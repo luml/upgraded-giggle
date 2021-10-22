a850b17648f1c0b6ac5a020a7750dedcf074d175
luml <1520622848@qq.com>
2021-10-22 17:10:30 +0800

    :octocat: set up a hotel page

---
+++ src/components/Hotel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hotel = () => {
+  return (
+    <div>
+      This is a hotel status message manager, includes a form and a message list
+    </div>
+  )
+}
+
+export default Hotel
